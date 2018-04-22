39ca00d23c3f3044b5d3932c15ba4e2795dea7d1
Denis Klimenko <suncoin@gmail.com>
2018-04-22 12:08:44 +0300

    'test'

--- lecture01/src/main/java/ru.atom/HelloWorld.java
+++ lecture01/src/main/java/ru.atom/HelloWorld.java
@@ -2,9 +2,11 @@ package ru.atom;
 
 public class HelloWorld {
 
+
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
     }
+
 
     public static String getHelloWorld() {
         return "Hello, World!";
