43a5b9881e04e31eb138626241e9e53fa4b4a140
Denis Klimenko <suncoin@gmail.com>
2018-04-25 09:53:24 +0300

    my homework

--- lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
+++ lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
@@ -1,11 +1,18 @@
 package ru.atom.makejar;
 
+import java.util.Arrays;
+
 /**
  * Class just to test fat jar packing
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
+
+        int[] values = {10,4,5,9,12,4,7,9};
+        System.out.println(Arrays.stream(values).max().getAsInt());
+
+
     }
 
     public static String getHelloWorld() {
